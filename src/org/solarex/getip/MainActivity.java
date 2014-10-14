edd4deb4594086b86ad6e13884cc3c237f1c1ce8
Solarex <rh.hou.work@gmail.com>
2014-10-14 11:36:50 +0800

    exception check added, defensive programming

--- src/org/solarex/getip/MainActivity.java
+++ src/org/solarex/getip/MainActivity.java
@@ -3,20 +3,20 @@ package org.solarex.getip;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 
 import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.util.InetAddressUtils;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.solarex.getip.*;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -26,7 +26,6 @@ import java.net.MalformedURLException;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.URL;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -41,11 +40,29 @@ public class MainActivity extends Activity {
         tv_external = (TextView) this.findViewById(R.id.tv_external);
         tv_internal = (TextView) this.findViewById(R.id.tv_internal);
         mac = (TextView) this.findViewById(R.id.mac);
-        try {
-            new GetIPTask().execute(new URL("http://ifconfig.me/all.json"));
-        } catch (MalformedURLException e) {
-            Utils.log("Exception happened, ex = " + e.getMessage());
+
+        if (!isNetworkConnectd(this)) {
+            String networkMsg = "Network not connected.";
+            tv_external.setText("External IP: " + networkMsg);
+            tv_internal.setText("Internal IP: " + networkMsg);
+        } else {
+            try {
+                new GetIPTask().execute(new URL("http://ifconfig.me/all.json"));
+            } catch (MalformedURLException e) {
+                Utils.log("Exception happened, ex = " + e.getMessage());
+            }
         }
+    }
+
+    private boolean isNetworkConnectd(Context context) {
+        boolean isConnected = false;
+        ConnectivityManager cm =
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+        return isConnected;
     }
 
     private class GetIPTask extends AsyncTask<URL, Integer, HttpResponse> {
@@ -54,7 +71,7 @@ public class MainActivity extends Activity {
         @Override
         protected HttpResponse doInBackground(URL... params) {
             HttpResponse response = null;
-            //String url = "http://ifconfig.me/all.json";
+            // String url = "http://ifconfig.me/all.json";
             try {
                 HttpClient client = new DefaultHttpClient();
                 HttpGet request = new HttpGet(params[0].toString());
@@ -71,7 +88,8 @@ public class MainActivity extends Activity {
                             .getInetAddresses());
                     for (InetAddress inetAddress : inetAddresses) {
                         Utils.log(inetAddress.toString());
-                        if (!inetAddress.isLoopbackAddress()) {
+                        if (!inetAddress.isLoopbackAddress()
+                                && InetAddressUtils.isIPv4Address(inetAddress.getHostAddress())) {
                             internal = inetAddress.getHostAddress().toUpperCase();
                         }
                     }
@@ -110,20 +128,26 @@ public class MainActivity extends Activity {
         protected void onPostExecute(HttpResponse result) {
             super.onPostExecute(result);
             try {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(result.getEntity()
-                        .getContent()));
-                String line = null;
-                StringBuilder sBuilder = new StringBuilder();
-                while((line = reader.readLine()) != null ){
-                    sBuilder.append(line);
+                if (null != result) {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(result
+                            .getEntity()
+                            .getContent()));
+                    String line = null;
+                    StringBuilder sBuilder = new StringBuilder();
+                    while ((line = reader.readLine()) != null) {
+                        sBuilder.append(line);
+                    }
+                    reader.close();
+                    try {
+                        JSONObject jsonObject = new JSONObject(sBuilder.toString());
+                        external = jsonObject.getString("ip_addr");
+                    } catch (JSONException e) {
+                        Utils.log("Exception happened, ex = " + e.getMessage());
+                    }
+                } else {
+                    external = "External IP: " + " Network Exception happened.";
                 }
-                reader.close();
-                try {
-                    JSONObject jsonObject = new JSONObject(sBuilder.toString());
-                    external = jsonObject.getString("ip_addr");
-                } catch (JSONException e) {
-                    Utils.log("Exception happened, ex = " + e.getMessage());
-                }
+
             } catch (IllegalStateException e) {
                 Utils.log("Exception happened, ex = " + e.getMessage());
             } catch (IOException e) {
